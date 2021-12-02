2270627c42492f7d93368c3edc61e25706325bfc
deadem <deadem@gmail.com>
2021-12-02 11:55:53 +0300

    * more functional rust

--- 2021/1/a.rs
+++ 2021/1/a.rs
@@ -1,26 +1,16 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+// https://adventofcode.com/2021/day/1
 
 fn main() {
-    let mut count = 0;
-    if let Ok(lines) = read_lines("./input.txt") {
-        let mut last = i32::MAX;
-        for line in lines {
-            if let Ok(value) = line {
-                let number: i32 = value.parse().unwrap();
-                if number > last {
-                    count += 1;
-                }
-                last = number;
-                // println!("{}", number);
-            }
-        }
-    }
-    println!("{}", count);
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    println!("{}", 
+        include_str!("./input.txt")
+            .lines()
+            .map(|n| n.parse().unwrap())
+            .scan(i32::MAX, |last, x| {
+                let result = x > *last;
+                *last = x;
+                Some(result)
+            })
+            .filter(|x| *x)
+            .count()
+    );
 }
