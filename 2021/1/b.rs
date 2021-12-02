aa33cda2686b4d3cf6bd30de089dd38e10bc8a08
deadem <deadem@gmail.com>
2021-12-02 12:18:43 +0300

    + добавил функциональности

--- 2021/1/b.rs
+++ 2021/1/b.rs
@@ -1,32 +1,17 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+// https://adventofcode.com/2021/day/1
 use std::collections::VecDeque;
 
 fn main() {
-    let mut vec = VecDeque::<i32>::new();
-    let mut count = 0;
-    if let Ok(lines) = read_lines("./input.txt") {
-        let mut last = i32::MAX;
-        for line in lines {
-            if let Ok(value) = line {
-                vec.push_back(value.parse().unwrap());
-                if vec.len() >= 3 {
-                    let number = vec.iter().sum();
-                    vec.pop_front();
-
-                    if number > last {
-                        count += 1;
-                    }
-                    last = number;
-                }
-            }
-        }
-    }
-    println!("{}", count);
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    println!("{}", 
+        include_str!("./input.txt")
+            .lines()
+            .map(|n| n.parse().unwrap())
+            .scan(VecDeque::<i32>::new(), |last, x| {
+                last.push_back(x);
+                let first = if last.len() >= 4 { last.pop_front().unwrap() } else { i32::MAX };
+                Some(first < x)
+            })
+            .filter(|x| *x)
+            .count()
+    );
 }
