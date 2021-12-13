4f991d887896dfabe20e07d37255a81cf39f0282
deadem <deadem@gmail.com>
2021-12-13 17:33:13 +0300

    ? q1

--- 2021/day03/a.rs
+++ 2021/day03/a.rs
@@ -2,21 +2,23 @@
 use std::collections::HashMap;
 
 fn main() {
-    let (count, map) = include_str!("./input.txt")
+    let lines: Vec<Vec<char>> = include_str!("./input.txt")
         .lines()
         .map(|n| n.chars().collect())
-        .fold((0, HashMap::new()), |sum, v: Vec<char>| {
-            let (mut count, mut values) = sum;
-            count += 1;
+        .collect();
+
+    let map = lines.iter()
+        .fold(HashMap::new(), |mut values: HashMap<usize, i32>, v: &Vec<char>| {
             v.iter().enumerate().for_each(|(i, x)| if *x != '0' {
                 *values.entry(i).or_insert(0) += 1;
             });
-            (count, values)
+            values
         });
 
     let mut values: Vec<_> = map.into_iter().collect();
     values.sort_by(|a,b| a.0.cmp(&b.0));
 
+    let count = lines.len() as i32;
     let (gamma, epsilon)= values.iter().fold((0, 0), |(mut gamma, mut epsilon), (_, x)| {
         let number = if x >= &(count / 2) { 1 } else { 0 };
         gamma = gamma * 2 + number;
